f8b3d8956987b2e7d3fe3a369012b30d70511a2e
Ardi Fajar Maulana <ardifjr27@gmail.com>
2023-12-04 08:20:20 +0700

    fixed page bootstrap

--- resources/views/layout/mainlayout.blade.php
+++ resources/views/layout/mainlayout.blade.php
@@ -3,7 +3,7 @@
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-      <title>Testing DKPP</title>
+      <title>Itenas nih bos</title>
 
       <!-- Favicon -->
       <link rel="shortcut icon" type="image/x-icon" href="{{url('assets/img/favicon.png')}}">
